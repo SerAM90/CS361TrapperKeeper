7362a7c44574fbfe1fe614cae19979d8e94a94ff
Andrew <SerranoAM90@gmail.com>
2023-07-15 20:23:21 -0400

    Changed the window title, added instructions

--- TrapperKeeperGUI.py
+++ TrapperKeeperGUI.py
@@ -1,6 +1,7 @@
 import tkinter as tk
 import TrapperKeepPasswordGen as PasswordGenFunc
 window = tk.Tk()
+window.wm_title('TrapperKeeper')
 
 def generate_password(password_length):
     password_generated = PasswordGenFunc.generate_password(password_length)
@@ -9,15 +10,17 @@ def generate_password(password_length):
 
 
 password_output = tk.Label(text='')
-label = tk.Label(text='Python rock!')
+instructions = tk.Label(text='Instructions: TrapperKeeper is a random password generator! \n'
+                             'Push the "Generate 10 character Password", or "Generate 20 character Password" '
+                             'to create a password of 10 or 20 characters in length!')
 
-label.pack()
-password_output.pack()
+instructions.pack()
+
 
 
 
 ten_button = tk.Button(
-    text="Generate 10 character Password",
+    text="Generate 10 Character Password",
     width=25,
     height=5,
     bg="black",
@@ -25,7 +28,7 @@ ten_button = tk.Button(
     command=lambda: generate_password(10))
 
 twenty_button = tk.Button(
-    text="Generate 20 character Password",
+    text="Generate 20 Character Password",
     width=25,
     height=5,
     bg="black",
@@ -34,5 +37,5 @@ twenty_button = tk.Button(
 
 ten_button.pack()
 twenty_button.pack()
-
+password_output.pack()
 window.mainloop()
